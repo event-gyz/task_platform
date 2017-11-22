d33c910e68fb3145d1394a613b257c716b029387
hongyu_zhao <hongyu_zhao@eventown.com>
2017-11-22 14:03:18 +0800

    基本目录结构

---
+++ application/models/admin/Sys_user_model.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Class Sys_user_model
+ */
+class Sys_user_model extends MY_Model {
+
+    var $_table = 'sys_user';
+
+    public function __construct() {
+        parent::__construct();
+    }
+
+}
+
