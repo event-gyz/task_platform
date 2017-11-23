637c37661232184ce3f92ecfad7037bf3fa48714
hongyu_zhao <hongyu_zhao@eventown.com>
2017-11-23 16:35:06 +0800

    引入element-ui

--- application/views/admin/common/head_left_menu.php
+++ application/views/admin/common/head_left_menu.php
@@ -13,8 +13,18 @@
                     <span class="pull-right-container"> <i class="fa fa-angle-left pull-right"></i> </span>
                 </a>
                 <ul class="treeview-menu">
-                    <li><a href="/admin/index/home1">个人广告主审核列表</a></li>
-                    <li class="active"><a href="/admin/index/home2">公司广告主审核列表</a></li>
+                    <li><a href="/admin/index/home1">个人审核列表</a></li>
+                    <li class="active"><a href="/admin/index/home2">公司审核列表</a></li>
+                </ul>
+            </li>
+
+            <li class="treeview">
+                <a href="#"><i class="fa fa-link"></i>
+                    <span>自媒体人管理</span>
+                    <span class="pull-right-container"> <i class="fa fa-angle-left pull-right"></i> </span>
+                </a>
+                <ul class="treeview-menu">
+                    <li><a href="/admin/index/home1">自媒体人审核列表</a></li>
                 </ul>
             </li>
 
