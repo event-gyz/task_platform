863e2efc5c76216e19a6210a4fa5470004b80ec2
hongyu_zhao <hongyu_zhao@eventown.com>
2017-11-23 16:33:26 +0800

    引入element-ui

--- application/views/admin/index/index.php
+++ application/views/admin/index/index.php
@@ -21,6 +21,21 @@
           -------------------------->
         这里是页面的内容
 
+        <div id="app">
+            <el-button @click="visible = true">按钮</el-button>
+            <el-dialog :visible.sync="visible" title="Hello world">
+                <p>欢迎使用 Element</p>
+            </el-dialog>
+
+            <el-button>默认按钮</el-button>
+            <el-button type="primary">主要按钮</el-button>
+            <el-button type="success">成功按钮</el-button>
+            <el-button type="info">信息按钮</el-button>
+            <el-button type="warning">警告按钮</el-button>
+            <el-button type="danger">危险按钮</el-button>
+
+        </div>
+
     </section>
     <!-- /.content -->
 </div>
@@ -28,5 +43,14 @@
 
 
 <?php include VIEWPATH . '/admin/common/foot.php' ?>
+
+<script>
+    new Vue({
+        el: '#app',
+        data: function() {
+            return { visible: false }
+        }
+    })
+</script>
 
 </html>
