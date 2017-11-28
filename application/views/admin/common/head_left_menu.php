8da6842c98afb726b5dba69d5d51c5c55cabd495
hongyu_zhao <hongyu_zhao@eventown.com>
2017-11-28 18:29:22 +0800

    根据权限展示菜单列表

--- application/views/admin/common/head_left_menu.php
+++ application/views/admin/common/head_left_menu.php
@@ -1,5 +1,6 @@
 <?php
 
+// 菜单列表
 $s_auth_list = $_SESSION['menu_auth_list'];
 
 /**
@@ -16,6 +17,10 @@ function get_active_str($cur_path) {
     return ($pos === false) ? '' : 'active';
 }
 
+// 用户的权限列表
+$auth_ids    = $_SESSION['sys_user_info']['auth_ids'];
+$auth_id_arr = explode(',', $auth_ids);
+
 ?>
 
 <!-- Left side column. contains the logo and sidebar -->
@@ -30,7 +35,7 @@ function get_active_str($cur_path) {
             <?php foreach ($s_auth_list as $value0): ?>
                 <li class="treeview">
 
-                    <?php if ($value0['level'] === '0'): ?>
+                    <?php if (($value0['level'] === '0') && (in_array($value0['id'], $auth_id_arr))): ?>
                         <a href="javascript:;">
                             <span><?= $value0['auth_name'] ?></span>
                             <span class="pull-right-container">
@@ -41,7 +46,7 @@ function get_active_str($cur_path) {
 
                     <ul class="treeview-menu">
                         <?php foreach ($s_auth_list as $value1): ?>
-                            <?php if ($value1['level'] === '1' && $value1['pid'] === $value0['id']): ?>
+                            <?php if (($value1['level'] === '1') && ($value1['pid'] === $value0['id']) && (in_array($value1['id'], $auth_id_arr))): ?>
                                 <li
                                         class="<?= get_active_str("/{$value1['class']}") ?>"
                                 >
