7e9dfcfa43fe067e7d655e25c908bb3edf5b5d56
hongyu_zhao <hongyu_zhao@eventown.com>
2017-11-29 18:36:47 +0800

    1

--- application/views/admin/platform_advertiser/company_adv_detail.php
+++ application/views/admin/platform_advertiser/company_adv_detail.php
@@ -14,145 +14,114 @@
     </section>
 
     <!-- Main content -->
-    <section class="content container-fluid invoice">
+    <section class="content container-fluid">
 
-        <!-- title row -->
-        <div class="row">
-            <div class="col-xs-12">
-                <h2 class="page-header">
-                    <i class="fa fa-globe"></i> 北京会唐世纪科技有限公司
-                    <small class="pull-right">Date: 2/10/2014</small>
-                </h2>
+        <div class="box box-default">
+            <div class="box-header with-border">
+                <h3 class="box-title">基本信息</h3>
             </div>
-            <!-- /.col -->
-        </div>
-        <!-- info row -->
-        <div class="row invoice-info">
-            <div class="col-sm-4 invoice-col">
-                From
-                <address>
-                    <strong>Admin, Inc.</strong><br>
-                    795 Folsom Ave, Suite 600<br>
-                    San Francisco, CA 94107<br>
-                    Phone: (804) 123-5432<br>
-                    Email: info@almasaeedstudio.com
-                </address>
-            </div>
-            <!-- /.col -->
-            <div class="col-sm-4 invoice-col">
-                To
-                <address>
-                    <strong>John Doe</strong><br>
-                    795 Folsom Ave, Suite 600<br>
-                    San Francisco, CA 94107<br>
-                    Phone: (555) 539-1037<br>
-                    Email: john.doe@example.com
-                </address>
-            </div>
-            <!-- /.col -->
-            <div class="col-sm-4 invoice-col">
-                <b>Invoice #007612</b><br>
-                <br>
-                <b>Order ID:</b> 4F3S8J<br>
-                <b>Payment Due:</b> 2/22/2014<br>
-                <b>Account:</b> 968-34567
-            </div>
-            <!-- /.col -->
-        </div>
-        <!-- /.row -->
+            <div class="box-body">
 
-        <!-- Table row -->
-        <div class="row">
-            <div class="col-xs-12 table-responsive">
-                <table class="table table-striped">
-                    <thead>
-                    <tr>
-                        <th>Qty</th>
-                        <th>Product</th>
-                        <th>Serial #</th>
-                        <th>Description</th>
-                        <th>Subtotal</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>Call of Duty</td>
-                        <td>455-981-221</td>
-                        <td>El snort testosterone trophy driving gloves handsome</td>
-                        <td>$64.50</td>
-                    </tr>
-                    <tr>
-                        <td>1</td>
-                        <td>Need for Speed IV</td>
-                        <td>247-925-726</td>
-                        <td>Wes Anderson umami biodiesel</td>
-                        <td>$50.00</td>
-                    </tr>
-                    <tr>
-                        <td>1</td>
-                        <td>Monsters DVD</td>
-                        <td>735-845-642</td>
-                        <td>Terry Richardson helvetica tousled street art master</td>
-                        <td>$10.70</td>
-                    </tr>
-                    <tr>
-                        <td>1</td>
-                        <td>Grown Ups Blue Ray</td>
-                        <td>422-568-642</td>
-                        <td>Tousled lomo letterpress</td>
-                        <td>$25.99</td>
-                    </tr>
-                    </tbody>
-                </table>
-            </div>
-            <!-- /.col -->
-        </div>
-        <!-- /.row -->
+                <div class="row">
 
-        <div class="row">
-            <!-- accepted payments column -->
-            <div class="col-xs-6">
-                <p class="lead">Payment Methods:</p>
-                <img src="../../dist/img/credit/visa.png" alt="Visa">
-                <img src="../../dist/img/credit/mastercard.png" alt="Mastercard">
-                <img src="../../dist/img/credit/american-express.png" alt="American Express">
-                <img src="../../dist/img/credit/paypal2.png" alt="Paypal">
+                    <div class="col-sm-3 invoice-col">
+                        <b>用户ID：</b> <?= $info['advertiser_id'] ?><br><br>
+                    </div>
 
-                <p class="text-muted well well-sm no-shadow" style="margin-top: 10px;">
-                    Etsy doostang zoodles disqus groupon greplin oooj voxy zoodles, weebly ning heekya handango imeem
-                    plugg
-                    dopplr jibjab, movity jajah plickers sifteo edmodo ifttt zimbra.
-                </p>
-            </div>
-            <!-- /.col -->
-            <div class="col-xs-6">
-                <p class="lead">Amount Due 2/22/2014</p>
+                    <div class="col-sm-3 invoice-col">
+                        <b>用户名：</b> <?= $info['advertiser_login_name'] ?><br><br>
+                    </div>
 
-                <div class="table-responsive">
-                    <table class="table">
-                        <tr>
-                            <th style="width:50%">Subtotal:</th>
-                            <td>$250.30</td>
-                        </tr>
-                        <tr>
-                            <th>Tax (9.3%)</th>
-                            <td>$10.34</td>
-                        </tr>
-                        <tr>
-                            <th>Shipping:</th>
-                            <td>$5.80</td>
-                        </tr>
-                        <tr>
-                            <th>Total:</th>
-                            <td>$265.24</td>
-                        </tr>
-                    </table>
+                    <div class="col-sm-3 invoice-col">
+                        <b>公司名称：</b> <?= $info['company_name'] ?><br><br>
+                    </div>
+
+                    <div class="col-sm-3 invoice-col">
+                        <b>公司地址：</b> <?= $info['company_address'] ?><br><br>
+                    </div>
+
                 </div>
+
+                <div class="row">
+
+                    <div class="col-sm-3 invoice-col">
+                        <b>联系人姓名：</b> <?= $info['content_name'] ?><br><br>
+                    </div>
+
+                    <div class="col-sm-3 invoice-col">
+                        <b>联系人电话：</b> <?= $info['content_phone'] ?><br><br>
+                    </div>
+
+                    <div class="col-sm-3 invoice-col">
+                        <b>注册时间：</b> <?= $info['create_time'] ?><br><br>
+                    </div>
+
+                    <div class="col-sm-3 invoice-col">
+                        <b>审核状态：</b> <?= $info['audit_status'] ?><br><br>
+                    </div>
+
+                </div>
+
+                <div class="row">
+                    <div class="col-sm-3">
+                        <b>账户状态：</b> <?= $info['status'] ?><br><br>
+                    </div>
+                    <div class="col-sm-3">
+                        <b>营业执照：</b>
+                        <a href="<?= $info['business_license_pic'] ?>" target="_blank">
+                            <img src="<?= $info['business_license_pic'] ?>"
+                                 alt="<?= $info['company_name'] ?>"
+                                 class="img-thumbnail">
+                        </a>
+                        <br><br>
+                    </div>
+                </div>
+
             </div>
-            <!-- /.col -->
+            <!-- /.box-body -->
         </div>
-        <!-- /.row -->
+
+        <div class="box box-default">
+            <div class="box-header with-border">
+                <h3 class="box-title">操作日志</h3>
+            </div>
+            <div class="box-body">
+
+                <!-- Table row -->
+                <div class="row">
+                    <div class="col-xs-12 table-responsive">
+                        <table class="table table-striped">
+                            <thead>
+                            <tr>
+                                <th>序号</th>
+                                <th>用户名</th>
+                                <th>操作时间</th>
+                                <th>操作事项</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>Call of Duty</td>
+                                <td>455-981-221</td>
+                                <td>El snort testosterone trophy driving gloves handsome</td>
+                            </tr>
+                            <tr>
+                                <td>1</td>
+                                <td>Need for Speed IV</td>
+                                <td>247-925-726</td>
+                                <td>Wes Anderson umami biodiesel</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <!-- /.col -->
+                </div>
+                <!-- /.row -->
+
+            </div>
+            <!-- /.box-body -->
+        </div>
 
         <div class="row">
             <div class="col-xs-12 col-xs-offset-5">
