e144fd7bfd16c27b31c9834ad79fe34294754081
hongyu_zhao <hongyu_zhao@eventown.com>
2017-11-29 16:35:07 +0800

    11

--- application/views/admin/platform_advertiser/personal_adv_home_index.php
+++ application/views/admin/platform_advertiser/personal_adv_home_index.php
@@ -209,7 +209,15 @@
 
 <script>
 
-    $('#reservation').daterangepicker();
+    $('#reservation').daterangepicker({
+        locale: {
+            applyLabel : '确定',
+            cancelLabel: '取消',
+            daysOfWeek : ['日', '一', '二', '三', '四', '五', '六'],
+            monthNames : ['一月', '二月', '三月', '四月', '五月', '六月',
+                '七月', '八月', '九月', '十月', '十一月', '十二月'],
+        }
+    });
 
     $('.del-department').click(function () {
         var del_url = $(this).attr('del-url');
