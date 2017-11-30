212092231ddfe79cbbabb708e39e2fab9d6f157b
hongyu_zhao <hongyu_zhao@eventown.com>
2017-11-30 16:32:59 +0800

    11

---
+++ application/views/admin/platform_advertiser/personal_adv_detail.php
@@ -0,0 +1,396 @@
+<!DOCTYPE html>
+<html>
+
+<?php include VIEWPATH . '/admin/common/head.php'; ?>
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper" v-loading.body="loading" element-loading-text="拼命加载中">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <h1>
+            个人广告主
+            <small>个人广告主审核详情页</small>
+        </h1>
+    </section>
+
+    <!-- Main content -->
+    <section class="content container-fluid">
+
+        <div class="box box-default">
+            <div class="box-header with-border">
+                <h3 class="box-title">基本信息</h3>
+            </div>
+            <div class="box-body">
+
+                <div class="row">
+
+                    <div class="col-sm-3 invoice-col">
+                        <b>用户ID：</b> <?= $info['advertiser_id'] ?><br><br>
+                    </div>
+
+                    <div class="col-sm-3 invoice-col">
+                        <b>用户名：</b> <?= $info['advertiser_login_name'] ?><br><br>
+                    </div>
+
+                    <div class="col-sm-3 invoice-col">
+                        <b>姓名：</b> <?= $info['advertiser_name'] ?><br><br>
+                    </div>
+
+                    <div class="col-sm-3 invoice-col">
+                        <b>身份证号：</b> <?= $info['id_card'] ?><br><br>
+                    </div>
+
+                </div>
+
+                <div class="row">
+
+                    <div class="col-sm-3 invoice-col">
+                        <b>手机号码：</b> <?= $info['advertiser_phone'] ?><br><br>
+                    </div>
+
+                    <div class="col-sm-3 invoice-col">
+                        <b>注册时间：</b> <?= $info['create_time'] ?><br><br>
+                    </div>
+
+                    <div class="col-sm-3 invoice-col">
+                        <b>审核状态：</b>
+                        <small class="label
+                                            <?= $info['audit_status'] === "0" ? "bg-yellow" : "" ?>
+                                            <?= $info['audit_status'] === "1" ? "bg-green" : "" ?>
+                                            <?= $info['audit_status'] === "2" ? "bg-red" : "" ?>
+                                        ">
+                            <?= $adv_audit_status[$info['audit_status']] ?>
+                        </small>
+                        <br><br>
+                    </div>
+
+                    <div class="col-sm-3">
+                        <b>账户状态：</b>
+                        <small class="label
+                                            <?= $info['status'] === "0" ? "bg-gray" : "" ?>
+                                            <?= $info['status'] === "1" ? "bg-yellow" : "" ?>
+                                            <?= $info['status'] === "2" ? "bg-green" : "" ?>
+                                            <?= $info['status'] === "9" ? "bg-red" : "" ?>
+                                        ">
+                            <?= $adv_account_status[$info['status']] ?>
+                        </small>
+
+                        <?php if ($info['status'] === "9"): ?>
+                            <button @click="update_adv_account_status('2','<?= $info['advertiser_id'] ?>')"
+                                    class="btn btn-success btn-sm" style="margin-left: 10px;">
+                                解冻
+                            </button>
+                        <?php endif; ?>
+
+                        <?php if ($info['status'] === "2"): ?>
+                            <button @click="update_adv_account_status('9','<?= $info['advertiser_id'] ?>')"
+                                    class="btn btn-danger btn-sm" style="margin-left: 10px;">
+                                冻结
+                            </button>
+                        <?php endif; ?>
+
+                        <br><br>
+                    </div>
+
+                </div>
+
+                <div class="row">
+
+                    <div class="col-sm-3">
+                        <b>身份证正面：</b>
+                        <a href="<?= $info['id_card_positive_pic'] ?>" target="_blank">
+                            <img src="<?= $info['id_card_positive_pic'] ?>"
+                                 alt="<?= $info['advertiser_name'] ?>"
+                                 class="img-thumbnail">
+                        </a>
+                        <br><br>
+                    </div>
+
+                    <div class="col-sm-3">
+                        <b>身份证反面：</b>
+                        <a href="<?= $info['id_card_back_pic'] ?>" target="_blank">
+                            <img src="<?= $info['id_card_back_pic'] ?>"
+                                 alt="<?= $info['advertiser_name'] ?>"
+                                 class="img-thumbnail">
+                        </a>
+                        <br><br>
+                    </div>
+
+                    <div class="col-sm-3">
+                        <b>身份证手持：</b>
+                        <a href="<?= $info['handheld_id_card_pic'] ?>" target="_blank">
+                            <img src="<?= $info['handheld_id_card_pic'] ?>"
+                                 alt="<?= $info['advertiser_name'] ?>"
+                                 class="img-thumbnail">
+                        </a>
+                        <br><br>
+                    </div>
+
+                </div>
+
+            </div>
+            <!-- /.box-body -->
+        </div>
+
+        <div class="box box-default">
+            <div class="box-header with-border">
+                <h3 class="box-title">操作日志</h3>
+            </div>
+            <div class="box-body">
+
+                <!-- Table row -->
+                <div class="row">
+                    <div class="col-xs-12 table-responsive">
+                        <table class="table table-striped">
+                            <thead>
+                            <tr>
+                                <th>序号</th>
+                                <th>用户名</th>
+                                <th>操作时间</th>
+                                <th>操作事项</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>Call of Duty</td>
+                                <td>455-981-221</td>
+                                <td>El snort testosterone trophy driving gloves handsome</td>
+                            </tr>
+                            <tr>
+                                <td>1</td>
+                                <td>Need for Speed IV</td>
+                                <td>247-925-726</td>
+                                <td>Wes Anderson umami biodiesel</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <!-- /.col -->
+                </div>
+                <!-- /.row -->
+
+            </div>
+            <!-- /.box-body -->
+        </div>
+
+        <?php if (in_array($info['audit_status'], [0, 2])): ?>
+
+            <div class="box box-default">
+                <div class="box-header with-border">
+                    <h3 class="box-title">审核操作</h3>
+                </div>
+                <div class="box-body">
+
+                    <div class="row">
+                        <div class="col-xs-12">
+
+                            <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px">
+
+                                <el-form-item label="审核结果" prop="audit_status">
+                                    <el-radio-group v-model="ruleForm.audit_status">
+                                        <el-radio label="1">通过</el-radio>
+                                        <el-radio label="2">不通过</el-radio>
+                                    </el-radio-group>
+                                </el-form-item>
+
+                                <el-form-item label="拒绝原因" prop="reasons_for_rejection">
+                                    <el-input placeholder="请填写拒绝的原因" type="textarea"
+                                              v-model="ruleForm.reasons_for_rejection"></el-input>
+                                </el-form-item>
+
+                                <el-form-item>
+                                    <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
+                                    <el-button @click="goBack('ruleForm')">返回</el-button>
+                                </el-form-item>
+
+                            </el-form>
+
+                        </div>
+                        <!-- /.col -->
+                    </div>
+
+                </div>
+                <!-- /.box-body -->
+            </div>
+
+        <?php endif; ?>
+
+    </section>
+    <!-- /.content -->
+    <div class="clearfix"></div>
+</div>
+<!-- /.content-wrapper -->
+
+<?php include VIEWPATH . '/admin/common/foot.php' ?>
+
+<script>
+
+    var Main = {
+        data   : function () {
+            return {
+                loading      : false,// 是否显示加载
+                advertiser_id: <?= $info['advertiser_id']?>,
+                ruleForm     : {
+                    audit_status         : '',
+                    reasons_for_rejection: ''
+                },
+                rules        : {
+                    audit_status         : [
+                        {required: true, message: '请选审核结果', trigger: 'change'}
+                    ],
+                    reasons_for_rejection: [
+                        {required: false, message: '请填写拒绝的原因', trigger: 'blur'}
+                    ]
+                }
+            };
+        },
+        methods: {
+            submitForm                  : function (formName) {
+                this.$refs[formName].validate((valid) => {
+
+                    if (!valid) {
+                        this.$message.error('请选审核结果');
+                        return false;
+                    }
+
+                    if (this.ruleForm.audit_status === "2") {
+                        if (this.ruleForm.reasons_for_rejection === "") {
+                            this.$message.error('请填写拒绝的原因');
+                            return false;
+                        }
+                    }
+
+                    this.adv_audit();
+                });
+            },
+            goBack                      : function (formName) {
+                window.location.href = '/admin/platform_advertiser/personal_adv_home';
+            },
+            adv_audit                   : async function () {
+                try {
+                    this.loading = true;
+                    var url      = '/admin/platform_advertiser/update_adv_audit_status';
+                    var response = await axios.post(
+                        url,
+                        {
+                            "id"                   : this.advertiser_id,
+                            "audit_status"         : this.ruleForm.audit_status,
+                            "reasons_for_rejection": this.ruleForm.reasons_for_rejection,
+                        },
+                    );
+                    this.loading = false;
+                    var resData  = response.data;
+
+                    if (resData.error_no === 0) {
+                        this.$message.success('操作成功,即将刷新页面...');
+                        return window.location.reload();
+                    }
+
+                    return this.$message.error(resData.msg);
+                }
+                catch (error) {
+
+                    this.loading = false;
+
+                    if (error instanceof Error) {
+
+                        if (error.response) {
+                            return this.$message.error(error.response.data.responseText);
+                        }
+
+                        if (error.request) {
+                            console.error(error.request);
+                            return this.$message.error('服务器未响应');
+                        }
+
+                        console.error(error);
+
+                    }
+
+                }
+            },
+            update_adv_account_status   : async function (account_status, advertiser_id) {
+
+                var message = (account_status === "2") ? "确定要将此用户解冻吗，解冻后可正常登陆使用。" : "确定要将此用户冻结吗，冻结后无法正常登陆。";
+
+                this.$confirm(message, '提示', {
+                    confirmButtonText: '确定',
+                    cancelButtonText : '取消',
+                    type             : 'warning'
+                }).then(async () => {
+
+                    var freezing_reason = "";
+
+                    if (account_status === "9") {
+
+                        await this.$prompt('请输入冻结的原因', '提示', {
+                            confirmButtonText: '确定',
+                            cancelButtonText : '取消',
+                            inputValidator   : (value) => { return value !== null; },
+                            inputErrorMessage: '冻结原因不能为空'
+                        }).then(({value}) => {
+                            freezing_reason = value;
+                        }).catch(() => {
+                        });
+
+                    }
+
+                    await this.do_update_adv_account_status(account_status, advertiser_id, freezing_reason);
+
+                }).catch(() => {
+                });
+
+            },
+            do_update_adv_account_status: async function (account_status, advertiser_id, freezing_reason) {
+                try {
+
+                    this.loading = true;
+                    var url      = '/admin/platform_advertiser/update_adv_account_status';
+                    var response = await axios.post(
+                        url,
+                        {
+                            "id"             : advertiser_id,
+                            "account_status" : account_status,
+                            "freezing_reason": freezing_reason,
+                        },
+                    );
+                    this.loading = false;
+                    var resData  = response.data;
+
+                    if (resData.error_no === 0) {
+                        this.$message.success('操作成功,即将刷新页面...');
+                        return window.location.reload();
+                    }
+
+                    return this.$message.error(resData.msg);
+
+                } catch (error) {
+
+                    this.loading = false;
+
+                    if (error instanceof Error) {
+
+                        if (error.response) {
+                            return this.$message.error(error.response.data.responseText);
+                        }
+
+                        if (error.request) {
+                            console.error(error.request);
+                            return this.$message.error('服务器未响应');
+                        }
+
+                        console.error(error);
+
+                    }
+
+                }
+            }
+        }
+    };
+    var Ctor = Vue.extend(Main);
+    new Ctor().$mount('#app')
+
+</script>
+
+</html>
