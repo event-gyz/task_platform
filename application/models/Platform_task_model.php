c4fce73c864b86693e2d520b1fcf1af0e00b9dd9
hongyu_zhao <hongyu_zhao@eventown.com>
2017-12-04 11:35:21 +0800

    11

--- application/models/Platform_task_model.php
+++ application/models/Platform_task_model.php
@@ -3,11 +3,11 @@
 /**
  * Class Platform_task_model
  */
-class Platform_task_model extends MY_Model{
+class Platform_task_model extends MY_Model {
 
     public $table = 'platform_task';
 
-    public function __construct(){
+    public function __construct() {
         parent::__construct();
     }
 
@@ -17,33 +17,45 @@ class Platform_task_model extends MY_Model{
         $sql = "SELECT [*] FROM `{$this->table}` AS pt where 1=1 ";
 
         // 拼接查询条件
+
         // 根据任务名称
+        if (isset($where['task_name']) && $where['task_name']) {
+            $sql .= sprintf(" AND pt.task_name like '%s%%'", $where['task_name']);
+        }
+
+        // 根据任务标题
         if (isset($where['title']) && $where['title']) {
             $sql .= sprintf(" AND pt.title like '%s%%'", $where['title']);
         }
+
         // 根据任务类型
         if (isset($where['task_type']) && $where['task_type']) {
             $sql .= sprintf(" AND pt.task_type = %d", $where['task_type']);
         }
-        // 根据任务状态
+
+        // 根据任务发布状态
         if (isset($where['release_status']) && $where['release_status']) {
             $sql .= sprintf(" AND pt.release_status = %d", $where['release_status']);
         }
-        // 根据审核状态
+
+        // 根据任务审核状态
         if (isset($where['audit_status']) && $where['audit_status']) {
             $sql .= sprintf(" AND pt.audit_status = %d", $where['audit_status']);
         }
+
         // 根据发布平台
         if (isset($where['publishing_platform']) && $where['publishing_platform']) {
-            $sql .= sprintf(" AND pt.status = %d", $where['publishing_platform']);
+            $sql .= sprintf(" AND pt.publishing_platform = '%s'", $where['publishing_platform']);
         }
+
         // 根据任务提交开始时间
         if (isset($where['start_time']) && $where['start_time']) {
-            $sql .= sprintf(" AND pt.create_time >= '%s'", $where['start_time']);
+            $sql .= sprintf(" AND pt.submit_audit_time >= '%s'", $where['start_time']);
         }
+
         // 根据任务提交结束时间
         if (isset($where['end_time']) && $where['end_time']) {
-            $sql .= sprintf(" AND pt.create_time <= '%s'", $where['end_time']);
+            $sql .= sprintf(" AND pt.submit_audit_time <= '%s'", $where['end_time']);
         }
 
         // 总数
@@ -72,13 +84,13 @@ class Platform_task_model extends MY_Model{
     }
 
 
-    public function updateInfo($task_id,$info){
-        $where = array('task_id'=>$task_id);
-        return $this->update($info, $where );
+    public function updateInfo($task_id, $info) {
+        $where = array('task_id' => $task_id);
+        return $this->update($info, $where);
     }
 
     public function selectById($task_id) {
-        if(empty($task_id)){
+        if (empty($task_id)) {
             return false;
         }
         $query = $this->db->get_where($this->getTableName(), array('task_id' => $task_id));
@@ -86,14 +98,14 @@ class Platform_task_model extends MY_Model{
     }
 
     public function selectByCondition($where) {
-        if(empty($where)){
+        if (empty($where)) {
             return false;
         }
         $query = $this->db->get_where($this->getTableName(), $where);
         return $query->row_array();
     }
 
-    public function insert($data){
+    public function insert($data) {
         $this->db->insert($this->table, $data);
         return $this->db->insert_id();
     }
@@ -102,7 +114,7 @@ class Platform_task_model extends MY_Model{
 
         $sql = "SELECT [*] FROM `{$this->table}` AS pt LEFT JOIN `platform_task_payment` AS ptp on pt.task_id=ptp.task_id where 1=1 ";
 
-        if(empty($where['advertiser_user_id'])){
+        if (empty($where['advertiser_user_id'])) {
             return false;
         }
         // 拼接查询条件
@@ -136,10 +148,10 @@ class Platform_task_model extends MY_Model{
 
         $sql = "SELECT [*] FROM `{$this->table}` AS pt LEFT JOIN `platform_task_payment` AS ptp on pt.task_id=ptp.task_id where 1=1 ";
 
-        if(empty($where['advertiser_user_id'])){
+        if (empty($where['advertiser_user_id'])) {
             return false;
         }
-        if(empty($where['task_id'])){
+        if (empty($where['task_id'])) {
             return false;
         }
         // 拼接查询条件
