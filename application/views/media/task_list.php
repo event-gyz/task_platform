d216e0a3e792d98d1a1749c8c362715b411f16e1
Zhangshengwu <shengwu_zhang@eventown.com>
2017-12-05 15:20:25 +0800

    渲染任务列表

--- application/views/media/task_list.php
+++ application/views/media/task_list.php
@@ -12,87 +12,35 @@
         <link rel="stylesheet" href="//cdn.bootcss.com/weui/1.1.1/style/weui.min.css">
     </head>
     <body>
-        <div class="main" id="app" style="margin: 0;">
-            <ul class="my_task">
+        <div class="main" id="app">
+            <ul class="my_task" style="display:none;">
                 <!--list-->
-                <li class="task_box">
+                <li class="task_box" v-for="item in lists">
                     <div class="top wait">
                         <p class="name">待领取</p>
-                        <p class="time">
-                            剩余：<span>12</span><b>小时</b><span>12</span><b>分</b>
-                        </p>
+                        <p class="time" v-html="item.allot_time"></p>
                     </div>
                     <div class="context">
                         <ul>
-                            <li>任务编号：<span>2017090908</span></li>
-                            <li>任务类型：<span>线上任务</span></li>
-                            <li>任务总价：<span class="warn">¥8888</span></li>
-                            <li>发布平台：<span>微博、微信</span></li>
-                            <li>任务时间：<span>11.14 ~ 11.06</span></li>
+                            <li>任务编号：<span>{{item.task_id}}</span></li>
+                            <li>任务类型：<span>{{typeFn(item.task_type)}}</span></li>
+                            <li>任务总价：<span class="warn">¥{{item.total_price}}</span></li>
+                            <li>发布平台：<span>{{platformFn(item.publishing_platform)}}</span></li>
+                            <li>任务时间：<span>{{timeFn(item.start_time,item.end_time)}}</span></li>
                         </ul>
                     </div>
                     <table class="task_table" style="width: 100%">
                         <tr>
-                            <td class="border"><a class="a_box pass" href="#">领取</a></td>
-                            <td><a class="a_box warn" href="#">拒绝</a></td>
+                            <td class="border"><a class="a_box pass" href="#" @click="pick(item.task_id)">领取</a></td>
+                            <td><a class="a_box warn" href="#" @click="reject(item.task_id)">拒绝</a></td>
                         </tr>
                     </table>
                 </li>
                 <!--list-end-->
-                <!--list-->
-                <li class="task_box">
-                    <div class="top wait">
-                        <p class="name">待领取</p>
-                        <p class="time">
-                            剩余：<span>12</span><b>小时</b><span>12</span><b>分</b>
-                        </p>
-                    </div>
-                    <div class="context">
-                        <ul>
-                            <li>任务编号：<span>2017090908</span></li>
-                            <li>任务类型：<span>线上任务</span></li>
-                            <li>任务总价：<span class="warn">¥8888</span></li>
-                            <li>发布平台：<span>微博、微信</span></li>
-                            <li>任务时间：<span>2017.11.14 ~ 2017.11.06</span></li>
-                        </ul>
-                    </div>
-                    <table class="task_table" style="width: 100%">
-                        <tr>
-                            <td class="border"><a class="a_box pass" href="#">领取</a></td>
-                            <td><a class="a_box warn" href="#">拒绝</a></td>
-                        </tr>
-                    </table>
-                </li>
-                <!--list-end-->
-                <!--list-->
-                <li class="task_box">
-                    <div class="top wait">
-                        <p class="name">待领取</p>
-                        <p class="time">
-                            剩余：<span>12</span><b>小时</b><span>12</span><b>分</b>
-                        </p>
-                    </div>
-                    <div class="context">
-                        <ul>
-                            <li>任务编号：<span>2017090908</span></li>
-                            <li>任务类型：<span>线上任务</span></li>
-                            <li>任务总价：<span class="warn">¥8888</span></li>
-                            <li>发布平台：<span>微博、微信</span></li>
-                            <li>任务时间：<span>2017.11.14 ~ 2017.11.06</span></li>
-                        </ul>
-                    </div>
-                    <table class="task_table" style="width: 100%">
-                        <tr>
-                            <td class="border"><a class="a_box pass" href="#">领取</a></td>
-                            <td><a class="a_box warn" href="#">拒绝</a></td>
-                        </tr>
-                    </table>
-                </li>
-                <!--list-end-->
+                <p class="no_task" style="display:none;"  v-show="!lists.length"><img src="/images/zwnr.png"><br>暂无分配的任务</p>
             </ul>
-            <p class="no_task"><img src="/images/zwnr.png"><br>暂无分配的任务</p>
             <!--滚动加载-->
-            <div class="weui-loadmore">
+            <div class="weui-loadmore" style="display:none;">
                 <i class="weui-loading"></i>
                 <span class="weui-loadmore__tips">正在加载</span>
             </div>
@@ -107,7 +55,7 @@
         <!--nav-end-->
         <script type="text/javascript" src="/js/third/zepto.min.js"></script>
         <script type="text/javascript" src="/js/third/vue.js"></script>
-
+        <script type="text/javascript" src="/js/third/moment.min.js"></script>
         <script src="http://cdn.bootcss.com/jquery-weui/1.0.1/js/jquery-weui.min.js"></script>
         <script type="text/javascript" src="/js/util.js"></script>
         <script type="text/javascript" src="/js/taskList.js"></script>
