e3dfc972e4793e6910090f08b2751ca38ff096d8
hongyu_zhao <hongyu_zhao@eventown.com>
2017-12-05 13:52:02 +0800

    1

--- application/views/admin/platform_task/task_detail.php
+++ application/views/admin/platform_task/task_detail.php
@@ -393,7 +393,15 @@
                 <?php if (in_array($info['audit_status'], [1])): ?>
                     <button @click="submitForm('ruleForm')" type="button" class="btn btn-success margin-r-5">提交</button>
                 <?php endif; ?>
-                <button type="button" class="btn btn-warning margin-r-5">手工作废</button>
+                <?php
+                // 是否显示手工作废按钮
+                $is_show_cancellation_btn = (in_array($info['audit_status'], [1, 2])) ||
+                    (($info['pay_status'] === "0") && ($info['audit_status'] === "3")) ||
+                    (($info['pay_status'] === "1") && ($info['audit_status'] === "3") && ($info['finance_status'] === "0"));
+                ?>
+                <?php if ($is_show_cancellation_btn): ?>
+                    <button type="button" class="btn btn-warning margin-r-5">手工作废</button>
+                <?php endif; ?>
                 <button @click="goBack('ruleForm')" type="button" class="btn btn-default margin-r-5"> 返回</button>
             </div>
         </div>
