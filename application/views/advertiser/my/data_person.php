3f021e258dc383d6c694b334c40cdc10c08256bf
关雅卓 <yazhuo_guan@eventown.com>
2017-12-06 12:52:28 +0800

    1111

--- application/views/advertiser/my/data_person.php
+++ application/views/advertiser/my/data_person.php
@@ -1,39 +1,40 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>项目名称</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=0">
-        <link rel="stylesheet" href="/css/common.css" />
-        <link rel="stylesheet" href="/css/index_advert.css" />
-    </head>
-    <body>
-        <div class="main">
-            <div class="login-style">
-                <div class="input-box">
-                    <table>
-                        <tr>
-                            <th align="left" class="border_bottom" width="80px;">类型</th>
-                            <td class="border_bottom" align="right">个人</td>
-                        </tr>
-                        <tr>
-                            <th align="left" class="border_bottom">姓名</th>
-                            <td class="border_bottom" align="right">孙悟空</td>
-                        </tr>
-                        <tr>
-                            <th align="left" class="border_bottom">手机号</th>
-                            <td align="right" class="border_bottom">18023232323</td>
-                        </tr>
-                        <tr>
-                            <th align="left">身份证号</th>
-                            <td align="right">14223332323345333</td>
-                        </tr>
-                    </table>
-                </div>
-            </div>
-
+<head>
+    <title>我的资料-个人</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=0">
+    <link rel="stylesheet" href="/css/common.css" />
+    <link rel="stylesheet" href="/css/indexAdvert.css" />
+</head>
+<body>
+<div class="main">
+    <div class="login-style">
+        <div class="input-box">
+            <table>
+                <tr>
+                    <?php $userInfo = $_SESSION['ad_user_info']?>
+                    <th align="left" class="border_bottom" width="80px;">类型</th>
+                    <td class="border_bottom" align="right">个人</td>
+                </tr>
+                <tr>
+                    <th align="left" class="border_bottom">姓名</th>
+                    <td class="border_bottom" align="right"><?=$userInfo['advertiser_name']?></td>
+                </tr>
+                <tr>
+                    <th align="left" class="border_bottom">手机号</th>
+                    <td align="right" class="border_bottom"><?=$userInfo['advertiser_phone']?></td>
+                </tr>
+                <tr>
+                    <th align="left">身份证号</th>
+                    <td align="right"><?=$userInfo['advertiser_name']?></td>
+                </tr>
+            </table>
         </div>
-        <script type="text/javascript" src="/js/third/jquery.js"></script>
-        <script type="text/javascript" src="/js/util.js"></script>
-    </body>
+    </div>
+
+</div>
+<script type="text/javascript" src="/js/third/jquery.js"></script>
+<script type="text/javascript" src="/js/util.js"></script>
+</body>
 </html>
--- application/views/advertiser/my/index.php
+++ application/views/advertiser/my/index.php
@@ -1,62 +1,62 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>我的</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=0">
-        <link rel="stylesheet" href="/css/common.css" />
-        <link rel="stylesheet" href="/css/indexAdvert.css" />
-        <link rel="stylesheet" href="//at.alicdn.com/t/font_15076_vo1nszstgavh1tt9.css">
-    </head>
-    <body>
-        <div class="main">
-            <div class="my">
-                <!--head-->
-                <div class="my_head">
-                    <ul>
-                        <li class="img"><Img src="/images/wx.png"></li>
-                        <li class="text">孙悟空</li>
-                    </ul>
-                </div>
-                <!--head-end-->
-                <table class="my_index_table">
-                    <tr onclick="window.location.href='./data_person.html'">
-                        <th align="center" width="35px">&nbsp;&nbsp;&nbsp;<img class="icon" src="/images/zl.png"></th>
-                        <td class="border_bottom">我的资料</td>
-                        <td align="right" class="border_bottom" width="30px"><i class="iconfont">&#xe610;</i></td>
-                    </tr>
-                    <tr onclick="window.location.href='/advertiser/index/message'">
-                        <th align="center">&nbsp;&nbsp;&nbsp;<img class="icon" src="/images/xx.png"></th>
-                        <td class="border_bottom">我的消息</td>
-                        <td align="right" class="border_bottom" width="30px;"><i class="iconfont">&#xe610;</i></td>
-                    </tr>
-                    <tr onclick="window.location.href='/advertiser/index/taskList'">
-                        <th align="center">&nbsp;&nbsp;&nbsp;<img class="icon" src="/images/rw.png"></th>
-                        <td class="border_bottom">我的任务</td>
-                        <td align="right" class="border_bottom" width="30px;"><i class="iconfont">&#xe610;</i></td>
-                    </tr>
-                    <tr onclick="window.location.href='../new_pwd.html'">
-                        <th align="center">&nbsp;&nbsp;&nbsp;<img class="icon" src="/images/mm.png"></th>
-                        <td class="border_bottom">修改密码</td>
-                        <td align="right" class="border_bottom" width="30px;"><i class="iconfont">&#xe610;</i></td>
-                    </tr>
-                    <tr onclick="window.location.href=''">
-                        <th align="center">&nbsp;&nbsp;&nbsp;<img class="icon" src="/images/kf.png"></th>
-                        <td>联系客服</td>
-                        <td align="right" width="30px;"><i class="iconfont">&#xe610;</i></td>
-                    </tr>
-                </table>
-                <p class="logout"><a href="#">退出登录</a></p>
-            </div>
+<head>
+    <title>我的</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=0">
+    <link rel="stylesheet" href="/css/common.css" />
+    <link rel="stylesheet" href="/css/indexAdvert.css" />
+    <link rel="stylesheet" href="//at.alicdn.com/t/font_15076_vo1nszstgavh1tt9.css">
+</head>
+<body>
+<div class="main">
+    <div class="my">
+        <!--head-->
+        <div class="my_head">
+            <ul>
+                <li class="img"><Img src="/images/wx.png"></li>
+                <li class="text">孙悟空</li>
+            </ul>
         </div>
-        <!--nav-->
-        <div class="nav">
-            <p><a href="/advertiser/index/home"><i class="iconfont">&#xe661;</i><br>首页</a></p>
-            <p><a href="/advertiser/index/taskView"><i class="iconfont">&#xe621;</i><br>开始推广</a></p>
-            <p class="cur"><a href="/advertiser/index/my"><i class="iconfont">&#xe600;</i><br>我的</a></p>
-        </div>
-        <!--nav-end-->
-        <script type="text/javascript" src="/js/third/jquery.js"></script>
-        <script type="text/javascript" src="/js/util.js"></script>
-    </body>
+        <!--head-end-->
+        <table class="my_index_table">
+            <tr onclick="window.location.href='/advertiser/index/userInfo'">
+                <th align="center" width="35px">&nbsp;&nbsp;&nbsp;<img class="icon" src="/images/zl.png"></th>
+                <td class="border_bottom">我的资料</td>
+                <td align="right" class="border_bottom" width="30px"><i class="iconfont">&#xe610;</i></td>
+            </tr>
+            <tr onclick="window.location.href='/advertiser/index/message'">
+                <th align="center">&nbsp;&nbsp;&nbsp;<img class="icon" src="/images/xx.png"></th>
+                <td class="border_bottom">我的消息</td>
+                <td align="right" class="border_bottom" width="30px;"><i class="iconfont">&#xe610;</i></td>
+            </tr>
+            <tr onclick="window.location.href='/advertiser/index/taskList'">
+                <th align="center">&nbsp;&nbsp;&nbsp;<img class="icon" src="/images/rw.png"></th>
+                <td class="border_bottom">我的任务</td>
+                <td align="right" class="border_bottom" width="30px;"><i class="iconfont">&#xe610;</i></td>
+            </tr>
+            <tr onclick="window.location.href='/advertiser/login/new_pwd'">
+                <th align="center">&nbsp;&nbsp;&nbsp;<img class="icon" src="/images/mm.png"></th>
+                <td class="border_bottom">修改密码</td>
+                <td align="right" class="border_bottom" width="30px;"><i class="iconfont">&#xe610;</i></td>
+            </tr>
+            <tr onclick="window.location.href=''">
+                <th align="center">&nbsp;&nbsp;&nbsp;<img class="icon" src="/images/kf.png"></th>
+                <td>联系客服</td>
+                <td align="right" width="30px;"><i class="iconfont">&#xe610;</i></td>
+            </tr>
+        </table>
+        <p class="logout"><a href="#">退出登录</a></p>
+    </div>
+</div>
+<!--nav-->
+<div class="nav">
+    <p><a href="/advertiser/index/home"><i class="iconfont">&#xe661;</i><br>首页</a></p>
+    <p><a href="/advertiser/index/taskView"><i class="iconfont">&#xe621;</i><br>开始推广</a></p>
+    <p class="cur"><a href="/advertiser/index/my"><i class="iconfont">&#xe600;</i><br>我的</a></p>
+</div>
+<!--nav-end-->
+<script type="text/javascript" src="/js/third/jquery.js"></script>
+<script type="text/javascript" src="/js/util.js"></script>
+</body>
 </html>
