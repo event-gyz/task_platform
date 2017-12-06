fd02c3e9e0dacd7d0b68031d6279695e5005fba7
关雅卓 <yazhuo_guan@eventown.com>
2017-12-06 09:06:14 +0800

    111

--- application/controllers/Index.php
+++ application/controllers/Index.php
@@ -11,14 +11,7 @@ class Index extends CI_Controller {
     public function home()
     {
 
-//        $this->load->library('page');
-//        $url                = '/order/companyOrder/order_list?';
-//        $page               = $this->page->get_page($total, $number, $url);
-//        $data['show_page']  = $page ['show_page'];
-        $this->load->model('Platform_task_payment_model');
-        $data = $this->Platform_task_payment_model->get_task_payment_list_by_condition(111);
-        echo '<pre>';print_r($data);exit;
-        $this->load->view('index/index');
+        redirect('/advertiser/index/home');
     }
 
     //发送短信，可用
