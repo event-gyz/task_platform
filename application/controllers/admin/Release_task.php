e74d5b4b12b7590b56ac4d65d76503b3da898707
hongyu_zhao <hongyu_zhao@eventown.com>
2017-12-06 14:41:40 +0800

    发布管理列表

--- application/controllers/admin/Release_task.php
+++ application/controllers/admin/Release_task.php
@@ -13,6 +13,100 @@ class Release_task extends ADMIN_Controller {
         parent::__construct();
     }
 
+    // 任务列表
+    public function home() {
+
+        $form_data = $this->__build_where_list();
+
+        $data = $this->__get_platform_task_model()->get_task_list_by_condition($form_data['where']);
+
+        $page_link = $this->get_page_link($data['total'], $form_data['where']['limit']);
+
+        return $this->load->view('admin/release_task/index',
+            [
+                'form_data'                => $form_data,
+                'list'                     => $data['list'],
+                'page_link'                => $page_link,
+                'publishing_platform_list' => $this->config->item('publishing_platform'),
+                'task_type_list'           => $this->config->item('task_type'),
+                'task_status_list'         => $this->config->item('task_status'),
+            ]
+        );
+    }
+
+    private function __build_where_list() {
+        $task_name           = $this->input->get('task_name', true);
+        $publishing_platform = $this->input->get('publishing_platform', true);
+        $task_type           = $this->input->get('task_type', true);
+        $submit_audit_time   = $this->input->get('submit_audit_time', true);
+        $task_status         = $this->input->get('task_status', true);
+
+        $where = [];
+        if (!empty($task_name)) {
+            $where['task_name'] = $task_name;
+        }
+
+        if (!empty($publishing_platform)) {
+            $where['publishing_platform'] = $publishing_platform;
+        }
+
+        if ($task_type !== '' && $task_type !== null) {
+            $where['task_type'] = $task_type;
+        }
+
+        if (!empty($submit_audit_time)) {
+            $time_arr            = explode(' - ', $submit_audit_time);
+            $where['start_time'] = date('Y-m-d H:i:s', strtotime($time_arr[0]));
+            $where['end_time']   = date('Y-m-d H:i:s', strtotime($time_arr[1] . "+1 day -1 seconds"));
+        }
+
+        if (!empty($task_status)) {
+
+            // '1' => '待审核',---> audit_status = 1
+            // '2' => '待广告主付款',---> pay_status = 0 && audit_status = 3
+            // '3' => '待财务确认',---> pay_status = 1 && audit_status = 3 && platform_task_payment.finance_status = 0
+            // '4' => '财务已确认',---> pay_status = 1 && audit_status = 3 && platform_task_payment.finance_status = 1
+            // '5' => '驳回',---> audit_status = 2
+
+            switch ($task_status) {
+                case 1:
+                    $where['audit_status'] = 1;
+                    break;
+                case 2:
+                    $where['pay_status']   = 0;
+                    $where['audit_status'] = 3;
+                    break;
+                case 3:
+                    $where['pay_status']     = 1;
+                    $where['audit_status']   = 3;
+                    $where['finance_status'] = 0;
+                    break;
+                case 4:
+                    $where['pay_status']     = 1;
+                    $where['audit_status']   = 3;
+                    $where['finance_status'] = 1;
+                    break;
+                case 5:
+                    $where['audit_status'] = 2;
+                    break;
+                default:
+            }
+
+        }
+
+        $page_arr = $this->get_list_limit_and_offset_params();
+        $where    = array_merge($page_arr, $where);
+
+        return [
+            'task_name'           => $task_name,
+            'publishing_platform' => $publishing_platform,
+            'task_type'           => $task_type,
+            'submit_audit_time'   => $submit_audit_time,
+            'task_status'         => $task_status,
+            'where'               => $where,
+        ];
+    }
+
     /**
      * @return Platform_task_model
      */
---
+++ application/views/admin/release_task/index.php
@@ -0,0 +1,228 @@
+<!DOCTYPE html>
+<html>
+
+<?php include VIEWPATH . '/admin/common/head.php'; ?>
+<link href="https://cdn.bootcss.com/bootstrap-daterangepicker/2.1.25/daterangepicker.min.css" rel="stylesheet">
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <h1>
+            任务审核列表
+            <small>管理任务的审核</small>
+        </h1>
+    </section>
+
+    <!-- Main content -->
+    <section class="content container-fluid">
+
+        <div class="row">
+
+            <div class="col-xs-12">
+
+                <div class="box box-success">
+
+                    <div class="box-body">
+
+                        <div class="row">
+
+                            <!-- form start -->
+                            <form class="form-horizontal">
+
+                                <div class="row">
+
+                                    <div class="form-group col-xs-3">
+                                        <label for="task_name" class="col-sm-3 control-label">任务名称</label>
+                                        <div class="col-sm-7">
+                                            <input type="text" class="form-control"
+                                                   placeholder="输入任务名称来搜索..." name="task_name"
+                                                   value="<?= $form_data['task_name'] ?>"
+                                            >
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group col-xs-3">
+                                        <label for="publishing_platform" class="col-sm-3 control-label">发布平台</label>
+                                        <div class="col-sm-7">
+                                            <select class="form-control" name="publishing_platform">
+                                                <option value="">全部</option>
+
+                                                <?php foreach ($publishing_platform_list as $key => $value): ?>
+                                                    <option value="<?= $key ?>"
+                                                        <?= "$key" === $form_data['publishing_platform'] ? 'selected' : ''; ?>
+                                                    >
+                                                        <?= $value ?>
+                                                    </option>
+                                                <?php endforeach; ?>
+
+                                            </select>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group col-xs-3">
+                                        <label for="task_type" class="col-sm-3 control-label">任务类型</label>
+                                        <div class="col-sm-7">
+                                            <select class="form-control" name="task_type">
+                                                <option value="">全部</option>
+
+                                                <?php foreach ($task_type_list as $key => $value): ?>
+                                                    <option value="<?= $key ?>"
+                                                        <?= "$key" === $form_data['task_type'] ? 'selected' : ''; ?>
+                                                    >
+                                                        <?= $value ?>
+                                                    </option>
+                                                <?php endforeach; ?>
+
+                                            </select>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group col-xs-3">
+                                        <label class="col-sm-3 control-label">提交时间</label>
+                                        <div class="input-group col-sm-7">
+                                            <div class="input-group-addon">
+                                                <i class="fa fa-calendar"></i>
+                                            </div>
+                                            <input type="text" name="submit_audit_time"
+                                                   class="form-control pull-right"
+                                                   id="reservation"
+                                                   value="<?= $form_data['submit_audit_time'] ?>"
+                                            >
+                                        </div>
+                                    </div>
+
+                                </div>
+
+                                <div class="row">
+
+                                    <div class="form-group col-xs-3">
+                                        <label for="task_status" class="col-sm-3 control-label">任务状态</label>
+                                        <div class="col-sm-7">
+                                            <select class="form-control" name="task_status">
+                                                <option value="">全部</option>
+
+                                                <?php foreach ($task_status_list as $key => $value): ?>
+                                                    <option value="<?= $key ?>"
+                                                        <?= "$key" === $form_data['task_status'] ? 'selected' : ''; ?>
+                                                    >
+                                                        <?= $value ?>
+                                                    </option>
+                                                <?php endforeach; ?>
+
+                                            </select>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group col-xs-3">
+                                        <label for="sex" class="col-sm-3 control-label"></label>
+                                        <div class="col-sm-7">
+                                            <button type="submit" class="btn btn-info">搜索</button>
+                                        </div>
+                                    </div>
+
+                                </div>
+
+                            </form>
+
+                        </div>
+
+                    </div>
+                    <!-- /.box-body -->
+
+                </div>
+
+                <div class="box">
+
+                    <div class="box-body table-responsive no-padding">
+                        <table class="table table-hover">
+                            <tr>
+                                <th>任务ID</th>
+                                <th>任务名称</th>
+                                <th>任务类型</th>
+                                <th>发布平台</th>
+                                <th>任务标题</th>
+                                <th>任务状态</th>
+                                <th>提交时间</th>
+                                <th>提交人</th>
+                                <th>期望帐号数量</th>
+                                <th>系统匹配数量</th>
+                                <th>操作</th>
+                            </tr>
+
+                            <?php foreach ($list as $value): ?>
+                                <tr>
+                                    <th><?= $value['task_id'] ?></th>
+                                    <th><?= $value['task_name'] ?></th>
+                                    <th><?= $task_type_list[$value['task_type']] ?></th>
+                                    <th>
+                                        <?php foreach (explode(',', $value['publishing_platform']) as $key2 => $value2): ?>
+                                            <?= $publishing_platform_list[$value2] ?>
+                                        <?php endforeach; ?>
+                                    </th>
+                                    <th><?= $value['title'] ?></th>
+                                    <th>
+                                        <?php if (($value['audit_status'] === "1")): ?>
+                                            待审核
+                                        <?php elseif (($value['audit_status'] === "2")): ?>
+                                            驳回
+                                        <?php elseif (($value['pay_status'] === "0") && ($value['audit_status'] === "3")): ?>
+                                            待广告主付款
+                                        <?php elseif (($value['pay_status'] === "1") && ($value['audit_status'] === "3") && ($value['finance_status'] === "0")): ?>
+                                            待财务确认
+                                        <?php elseif (($value['pay_status'] === "1") && ($value['audit_status'] === "3") && ($value['finance_status'] === "1")): ?>
+                                            财务已确认
+                                        <?php else: ?>
+                                            未知
+                                        <?php endif; ?>
+                                    </th>
+                                    <th><?= $value['submit_audit_time'] ?></th>
+                                    <th><?= $value['advertiser_user_name'] ?></th>
+                                    <th><?= $value['media_man_number'] ?></th>
+                                    <th><?= $value['actual_media_man_number'] ?></th>
+                                    <th>
+                                        <a href="/admin/platform_task/task_detail?id=<?= $value['task_id'] ?>"
+                                           class="btn btn-success btn-sm">
+                                            详情
+                                        </a>
+                                    </th>
+                                </tr>
+                            <?php endforeach; ?>
+                        </table>
+                    </div>
+                    <!-- /.box-body -->
+
+                    <?= $page_link ?>
+
+                </div>
+                <!-- /.box -->
+
+            </div>
+
+        </div>
+
+    </section>
+    <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+
+<?php include VIEWPATH . '/admin/common/foot.php' ?>
+
+<script src="https://cdn.bootcss.com/bootstrap-daterangepicker/2.1.25/moment.min.js"></script>
+<script src="https://cdn.bootcss.com/bootstrap-daterangepicker/2.1.25/daterangepicker.min.js"></script>
+
+<script>
+
+    $('#reservation').daterangepicker({
+        locale: {
+            applyLabel : '确定',
+            cancelLabel: '取消',
+            daysOfWeek : ['日', '一', '二', '三', '四', '五', '六'],
+            monthNames : ['一月', '二月', '三月', '四月', '五月', '六月',
+                '七月', '八月', '九月', '十月', '十一月', '十二月'],
+        }
+    });
+
+</script>
+
+</html>
