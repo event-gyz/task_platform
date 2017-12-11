e82d00298e4db0d14d48ad9c91b16ae536188c4b
关雅卓 <yazhuo_guan@eventown.com>
2017-12-11 17:45:59 +0800

    111

--- application/controllers/Jssdk.php
+++ application/controllers/Jssdk.php
@@ -48,7 +48,7 @@ class JSSDK  extends CI_Controller{
   private function getJsApiTicket() {
     // jsapi_ticket 应该全局存储与更新，以下代码以写入到文件中做示例
     $data = json_decode($this->get_php_file("http://campus.liuxuedai360.com/js/wx/jsapi_ticket.js"));
-    if ((time() - $data->expire_time) > 3600) {
+    if ($data->expire_time < time()) {
       $accessToken = $this->getAccessToken();
       // 如果是企业号用以下 URL 获取 ticket
       // $url = "https://qyapi.weixin.qq.com/cgi-bin/get_jsapi_ticket?access_token=$accessToken";
@@ -70,7 +70,7 @@ class JSSDK  extends CI_Controller{
   private function getAccessToken() {
     // access_token 应该全局存储与更新，以下代码以写入到文件中做示例
     $data = json_decode($this->get_php_file("http://campus.liuxuedai360.com/js/wx/access_token.js"));
-    if ((time() - $data->expire_time) > 3600) {
+    if ($data->expire_time <time()) {
       // 如果是企业号用以下URL获取access_token
       // $url = "https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=$this->appId&corpsecret=$this->appSecret";
       $url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=$this->appId&secret=$this->appSecret";
