1f9098680d960fb1cac75d426b765779a6ebec63
hongyu_zhao <hongyu_zhao@eventown.com>
2017-12-11 19:03:29 +0800

    上传文件

--- application/views/admin/platform_task_payment/index.php
+++ application/views/admin/platform_task_payment/index.php
@@ -317,7 +317,7 @@
             loading           : false,// 是否显示加载
             upload_loading    : false,// 上传的加载框
             dialogTableVisible: true,// 是否显示dialog
-            uploadUrl         : '/admin/login/upload_file',// 上传服务器地址
+            uploadUrl         : '/admin/platform_task_payment/upload_file',// 上传服务器地址
             upload_params     : {'task_id': 0},// 上传时附带的额外参数
             ruleForm          : {
                 pay_money     : '',
