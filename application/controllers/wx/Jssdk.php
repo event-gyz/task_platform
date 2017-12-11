9299b1a50e6067821c818c5e87dacf08d9a083cd
关雅卓 <yazhuo_guan@eventown.com>
2017-12-11 16:36:16 +0800

    1

--- application/controllers/wx/Jssdk.php
+++ application/controllers/wx/Jssdk.php
@@ -58,7 +58,7 @@ class JSSDK  extends CI_Controller{
       if ($ticket) {
         $data->expire_time = time() + 7000;
         $data->jsapi_ticket = $ticket;
-        $this->set_php_file("../../webroot/js/wx/jsapi_ticket.php", json_encode($data));
+        $this->set_php_file("./../../webroot/js/wx/jsapi_ticket.php", json_encode($data));
       }
     } else {
       $ticket = $data->jsapi_ticket;
@@ -79,7 +79,7 @@ class JSSDK  extends CI_Controller{
       if ($access_token) {
         $data->expire_time = time() + 7000;
         $data->access_token = $access_token;
-        $this->set_php_file("../../webroot/js/wx/access_token.php", json_encode($data));
+        $this->set_php_file("./../../webroot/js/wx/access_token.php", json_encode($data));
       }
     } else {
       $access_token = $data->access_token;
