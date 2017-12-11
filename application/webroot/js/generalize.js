c3f6bea0fd0318e0ed5886d0d98a02726a59b5d2
Zhangshengwu <shengwu_zhang@eventown.com>
2017-12-11 18:35:10 +0800

    push

--- application/webroot/js/generalize.js
+++ application/webroot/js/generalize.js
@@ -183,7 +183,8 @@ var app = new Vue({
                 sourceType: ['album', 'camera'],
                 success: function (res) {
                     var localIds = res.localIds.split(','); // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
-                    _this.taskImg.concat(localIds);
+                    _this.taskImg = _this.taskImg.concat(localIds);
+                    alert(_this.taskImg.length);
                 }
             });
         },
