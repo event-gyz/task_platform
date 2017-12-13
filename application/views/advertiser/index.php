d25bef094ab8cc59d429d586a5ed14dd93ef47fa
Zhangshengwu <shengwu_zhang@eventown.com>
2017-12-13 16:06:37 +0800

    push

--- application/views/advertiser/index.php
+++ application/views/advertiser/index.php
@@ -5,7 +5,6 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=0">
         <link rel="stylesheet" href="/css/common.css" />
-        <link rel="stylesheet" href="/css/swiper-3.4.2.min.css" />
         <link rel="stylesheet" href="/css/indexAdvert.css" />
     </head>
     <body>
