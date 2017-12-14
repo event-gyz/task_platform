32651548ead898319072b9ed399600f6e6382667
hongyu_zhao <hongyu_zhao@eventown.com>
2017-12-14 14:08:37 +0800

    广告主任务审核通过的通知

--- application/language/chinese/message_lang.php
+++ application/language/chinese/message_lang.php
@@ -11,3 +11,9 @@ $lang['media_audit_reject4_sys']  = '自媒体人审核被驳回,原因是：%s'
 $lang['media_audit_pass4_sys']    = '自媒体人审核通过';
 $lang['media_audit_reject4_user'] = '抱歉,您的资质未能通过审核,原因是：%s';
 $lang['media_audit_pass4_user']   = '恭喜，您的资质审核已通过，可正常使用各项功能。';
+
+// 广告主提交的任务审核
+$lang['adv_task_audit_reject4_sys']  = '广告主提交的任务被驳回,原因是：%s';
+$lang['adv_task_audit_pass4_sys']    = '广告主提交的任务审核通过';
+$lang['adv_task_audit_reject4_user'] = '抱歉,您的任务《%s》审核未通过,原因是：%s';
+$lang['adv_task_audit_pass4_user']   = '您的任务《%s》已经审核通过，赶快去付款吧。';
