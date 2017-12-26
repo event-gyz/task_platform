a9d2401fb84d80ff678f69a56d48149f1d21b051
hongyu_zhao <hongyu_zhao@eventown.com>
2017-12-26 15:47:30 +0800

    index页跳转

--- application/controllers/Index.php
+++ application/controllers/Index.php
@@ -3,22 +3,19 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Index extends CI_Controller {
 
-    public function __construct()
-    {
+    public function __construct() {
         parent::__construct();
     }
 
-    public function home()
-    {
-
-        redirect('/advertiser/index/home');
-
+    public function home() {
+        $this->load->helper(array('url', 'wap'));
+        return redirect(wap::get_server_address_and_port() . '/advertiser/index/home');
     }
 
-    //发送短信，可用
-    public function testsms(){
-//        $this->load->library('JSMS');
-//        $this->JSMS = new JSMS();
-//        $this->JSMS->sendMessage('15710061246',1,['code'=>'4178']);
+    // 发送短信，可用
+    public function testsms() {
+        // $this->load->library('JSMS');
+        // $this->JSMS = new JSMS();
+        // $this->JSMS->sendMessage('15710061246',1,['code'=>'4178']);
     }
 }
